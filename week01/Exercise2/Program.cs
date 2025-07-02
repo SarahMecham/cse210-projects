1a3981789dc6339920b3d5696c3c86106ee2a916
SarahMecham <sarahdmecham@gmail.com>
2025-07-02 17:38:35 -0600

    Exercise 2 completed.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What was your grade percentage? ");
+        string userInput = Console.ReadLine();
+        int grade = int.Parse(userInput);
+        int onesPlace = grade % 10;
+
+        string letter = "";
+        string plusMinus = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade < 90 && grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade < 80 && grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade < 70 && grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (onesPlace >= 7)
+        {
+            plusMinus = "+";
+        }
+        else if (onesPlace < 3)
+        {
+            plusMinus = "-";
+        }
+        else
+        {
+            plusMinus = "";
+        }
+
+        Console.WriteLine($"Your letter grade is {letter}{plusMinus}.");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations, you passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("Keep trying. You'll get it next time!");
+        }
     }
 }
