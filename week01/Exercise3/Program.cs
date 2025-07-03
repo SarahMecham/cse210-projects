46baf2084a710f4bde71dae3bfebd8b086569b58
SarahMecham <sarahdmecham@gmail.com>
2025-07-02 20:29:36 -0600

    Completed the stretch challenges for exercise 3.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -9,13 +9,15 @@ class Program
         do
         {
             Random randomGenerator = new Random();
-            int magicNumber = randomGenerator.Next(1, 20);
+            int magicNumber = randomGenerator.Next(1, 101);
             int guess = 0;
+            int attempts = 0;
 
             while (guess != magicNumber)
             {
                 Console.Write("What is your guess? ");
                 guess = int.Parse(Console.ReadLine());
+                attempts++;
 
                 if (guess < magicNumber)
                 {
@@ -27,7 +29,7 @@ class Program
                 }
                 else
                 {
-                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You guessed it! It took you {attempts} tries.");
                     Console.Write("Do you want to play again? ");
                     response = Console.ReadLine();
                 }
