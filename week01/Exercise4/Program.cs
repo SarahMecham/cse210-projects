c490d93bef91ef16eb4f20e58d35829a5033e726
SarahMecham <sarahdmecham@gmail.com>
2025-07-04 16:49:10 -0600

    Step 1 of exercise 4 complete.

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,28 @@
 using System;
+using System.Globalization;
+using System.Transactions;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> numbers = new List<int>();
+        int number;
+
+        do
+        {
+            Console.Write("Enter number: ");
+            string input = Console.ReadLine();
+            number = int.Parse(input);
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
+        int total = numbers.Sum();
+        Console.WriteLine($"The sum is: {total}.");
     }
 }
