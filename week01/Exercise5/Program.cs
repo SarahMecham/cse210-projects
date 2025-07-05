2fbb4874148a40f2a2867b5e323147cc83faf1f1
SarahMecham <sarahdmecham@gmail.com>
2025-07-05 13:35:44 -0600

    Completed exercise 5.

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection.Metadata.Ecma335;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        welcomeMessage();
+
+        string UserName = getUserName();
+        int number = getNumber();
+
+        endMessage(UserName, number);
+
+        static void welcomeMessage()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        static string getUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int getNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            string input = Console.ReadLine();
+            int favoriteNumber = int.Parse(input);
+            int favNumSquare = favoriteNumber * favoriteNumber;
+            return favNumSquare;
+        }
+
+        static void endMessage(string UserName, int number)
+        {
+            Console.WriteLine($"{UserName}, the square of your number is {number}.");
+        }
     }
 }
