96d26ff009484a85926d86e5ba2810b64ddd04f2
SarahMecham <sarahdmecham@gmail.com>
2025-11-10 10:55:08 -0700

    Finished week 3 learning activity

--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction one = new Fraction();
+        Console.WriteLine(one.GetFractionString());
+        Console.WriteLine(one.GetDecimalValue());
+
+        Fraction two = new Fraction(5);
+        Console.WriteLine(two.GetFractionString());
+        Console.WriteLine(two.GetDecimalValue());
+
+        Fraction three = new Fraction(3, 4);
+        Console.WriteLine(three.GetFractionString());
+        Console.WriteLine(three.GetDecimalValue());
+
+        three.SetTop(1);
+        three.SetBottom(3);
+        Console.WriteLine(three.GetFractionString());
+        Console.WriteLine(three.GetDecimalValue());
     }
 }
